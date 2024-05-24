1414182c723ad04bac1c521a04c4223d76109d12
vitatriutami <vitatriutami@gmail.com>
2024-05-24 10:09:17 +0700

    adding button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="w-screen h-screen flex justify-center m-auto">
+      <div className="p-10 m-auto">
+        <button className="border border-neutral-300 rounded-lg py-1.5 px-10 my-2 bg-blue-500 hover:bg-blue-600 text-white">
+          Open
+        </button>
+      </div>
+    </main>
+  );
 }
